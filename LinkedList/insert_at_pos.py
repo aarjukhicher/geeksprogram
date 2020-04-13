6ae4982f47c7c54fe465370bba9926ff22aaa4b2
Aarju <aarjukhicher@gmail.com>
2020-04-14 01:00:34 +0530

    linkedlist

--- LinkedList/insert_at_pos.py
+++ LinkedList/insert_at_pos.py
@@ -2,17 +2,22 @@ class Node:
     def __init__(self,data):
         self.data = data
         self.next = None
-
+    
 class Linkedlist:
     def __init__(self):
         self.head = None
-    def node_at_front(self,new_data):
+    
+    def insert_at(self,pre_node,new_data):
+        if pre_node is None:
+            print("wrong node given")
+            return
         new_node = Node(new_data)
-        new_node.next = self.head
-        self.head = new_node
+        new_node.next = pre_node.next
+        pre_node.next = new_node
     
     def printlist(self):
         temp = self.head
+
         while(temp):
             print(temp.data)
             temp = temp.next
@@ -21,5 +26,13 @@ if __name__=="__main__":
     llist = Linkedlist()
 
     llist.head = Node(1)
-    llist.node_at_front(input())
+    second = Node(2)
+    third = Node(3)
+    fourth = Node(4)
+
+    llist.head.next = second
+    second.next = third
+    third.next = fourth
+
+    llist.insert_at(second,5)
     llist.printlist()
