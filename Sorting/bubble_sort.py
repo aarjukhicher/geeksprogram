80c3f7d990416e3056c6ca8e614d54c2d44b9a56
Aarju <aarjukhicher@gmail.com>
2020-05-03 23:16:42 +0530

    selection sort

---
+++ Sorting/bubble_sort.py
@@ -0,0 +1,18 @@
+def bubblesort(array,n):
+    for j in range(n):
+        k = 0
+        for k in range(n-(j+1)) :
+            if array[k] > array[k+1] :
+                temp = array[k]
+                array[k] = array[k+1]
+                array[k+1] = temp 
+    print(array)
+if __name__=="__main__":
+    t = int(input("t "))
+    for i in range(t):
+        array = []
+        n = int(input("n "))
+        for j in range(n):
+            array.append(int(input("input ")))
+        print(array)
+        bubblesort(array,n)
